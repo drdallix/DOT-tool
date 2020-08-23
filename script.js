d6fda04585d0150f8f53ec3fea68998ce6e46b34
drdallix <69737869+drdallix@users.noreply.github.com>
2020-08-22 22:00:11 -0400

    using js library now instead of the ugly webfonts hack

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 function submit() {
-  var x = document.getElementById("input").value;
+  var z = document.getElementById("input").value;
   var y = "*"
   var z = y.concat(x,y)
-  
-  document.getElementById("output").innerHTML = z;
-
-}
-
-
-
-
-window.addEventListener("scroll", preventMotion, false);
-window.addEventListener("touchmove", preventMotion, false);
-
-function preventMotion(event)
-{
-    window.scrollTo(0, 0);
-    event.preventDefault();
-    event.stopPropagation();
+  console.log(x, z)
+  JsBarcode("#barcode", z);
 }
