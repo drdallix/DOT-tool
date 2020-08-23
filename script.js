771c2e7d9a36827981d2c01db6f7b08ddef905c6
drdallix <69737869+drdallix@users.noreply.github.com>
2020-08-22 23:24:06 -0400

    gg no re plz

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function submit() {
   console.log(input);
   JsBarcode("#barcode", input);
 }
+
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event) {
+  window.scrollTo(0, 0);
+  event.preventDefault();
+  event.stopPropagation();
+}
